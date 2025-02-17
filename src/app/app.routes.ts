decd9931e01a1e00a179e37895b02285e17ee8f2
imflawlezz <egorart6@gmail.com>
2025-02-17 12:17:55 +0300

    Added components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './features/home/home.component';
+import {MenuComponent} from './features/menu/menu.component';
+import {ContactsComponent} from './features/contacts/contacts.component';
+import {CartComponent} from './features/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent, pathMatch: 'full' },
+  { path: 'contacts', component: ContactsComponent, pathMatch: 'full' },
+  { path: 'cart', component: CartComponent, pathMatch: 'full' },
+];
---
+++ src/app/shared/components/dish-card/dish-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-card',
+  imports: [],
+  templateUrl: './dish-card.component.html',
+  styleUrl: './dish-card.component.scss'
+})
+export class DishCardComponent {
+
+}
