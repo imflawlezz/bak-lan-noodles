538f804c3b40c4cf27dfec05a53eb29128372025
imflawlezz <egorart6@gmail.com>
2025-02-17 17:33:00 +0300

    Finished home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {DishCardComponent} from '../../shared/components/dish-card/dish-card.component';
+import {NgForOf} from '@angular/common';
+import {NavComponent} from '../../shared/components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    DishCardComponent,
+    NgForOf,
+    NavComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  featuredDishes = [
+    {
+      name: 'Shrimp Noodles',
+      description: 'Nice and fishy',
+      price: 10.99,
+      image: '/assets/images/shrimp.png',
+    },
+    {
+      name: 'Spicy Noodles',
+      description: 'Hot stuff!',
+      price: 8.99,
+      image: '/assets/images/spicy.png',
+    },
+    {
+      name: 'Mushroom Noodles',
+      description: 'The legal ones',
+      price: 9.99,
+      image: '/assets/images/mushroom.png',
+    }
+  ];
 }
+
