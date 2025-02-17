25834e1c55b77d8299714813f2dac2b88b27464b
imflawlezz <egorart6@gmail.com>
2025-02-17 17:32:15 +0300

    Added dish-card component

--- src/app/shared/components/dish-card/dish-card.component.ts
+++ src/app/shared/components/dish-card/dish-card.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CartService} from '../../../core/services/cart.service';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-dish-card',
-  imports: [],
   templateUrl: './dish-card.component.html',
-  styleUrl: './dish-card.component.scss'
+  imports: [
+    NgIf,
+    NgOptimizedImage
+  ],
+  styleUrls: ['./dish-card.component.scss']
 })
 export class DishCardComponent {
+  @Input() dish: any;
 
+  constructor(private cartService: CartService) {}
+
+  toggleCart() {
+    if (this.cartService.isInCart(this.dish)) {
+      this.cartService.removeFromCart(this.dish);
+    } else {
+      this.cartService.addToCart(this.dish);
+    }
+  }
+
+  isInCart(): boolean {
+    return this.cartService.isInCart(this.dish);
+  }
 }
