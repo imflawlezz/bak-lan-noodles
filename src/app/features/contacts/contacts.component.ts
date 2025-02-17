a3d9d54f3febe6f23d747af954950970ed0465e3
imflawlezz <egorart6@gmail.com>
2025-02-17 18:54:09 +0300

    Added contacts page

--- src/app/features/contacts/contacts.component.ts
+++ src/app/features/contacts/contacts.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NavComponent} from '../../shared/components/nav/nav.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [
+    NavComponent,
+    RouterOutlet
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
