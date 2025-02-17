e5031bc937a460488d8345846734535048630196
imflawlezz <egorart6@gmail.com>
2025-02-17 17:33:29 +0300

    Added cart service

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+  private cartItems: any[] = []; // Здесь храним блюда в корзине
 
-  constructor() { }
+  addToCart(dish: any) {
+    if (!this.isInCart(dish)) {
+      this.cartItems.push(dish);
+    }
+  }
+
+  removeFromCart(dish: any) {
+    this.cartItems = this.cartItems.filter(item => item.name !== dish.name);
+  }
+
+  isInCart(dish: any): boolean {
+    return this.cartItems.some(item => item.name === dish.name);
+  }
 }
