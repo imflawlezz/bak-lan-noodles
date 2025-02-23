34e362d9c5456b2d402fc7552f3facf9747bc94d
imflawlezz <egorart6@gmail.com>
2025-02-23 23:21:53 +0100

    Adjusted dish-card for new font

--- src/app/shared/components/dish-card/dish-card.component.ts
+++ src/app/shared/components/dish-card/dish-card.component.ts
@@ -1,30 +1,29 @@
-import {Component, Input} from '@angular/core';
-import {CartService} from '../../../core/services/cart.service';
-import {NgIf, NgOptimizedImage} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CartService } from '../../../core/services/cart.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-dish-card',
   templateUrl: './dish-card.component.html',
   imports: [
-    NgIf,
-    NgOptimizedImage
+    NgIf
   ],
   styleUrls: ['./dish-card.component.scss']
 })
 export class DishCardComponent {
-  @Input() dish: any;
+  @Input() dish!: { name: string; description: string; price: number; image: string };
 
   constructor(private cartService: CartService) {}
 
+  isInCart(): boolean {
+    return this.cartService.isInCart(this.dish.name);
+  }
+
   toggleCart() {
-    if (this.cartService.isInCart(this.dish)) {
-      this.cartService.removeFromCart(this.dish);
+    if (this.isInCart()) {
+      this.cartService.removeFromCart(this.dish.name);
     } else {
       this.cartService.addToCart(this.dish);
     }
-  }
-
-  isInCart(): boolean {
-    return this.cartService.isInCart(this.dish);
   }
 }
