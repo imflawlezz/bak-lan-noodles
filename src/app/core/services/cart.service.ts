eaf97d755fb8184174a4ad4a6e2899c5f051042f
imflawlezz <egorart6@gmail.com>
2025-02-23 23:22:48 +0100

    New service for cart and more

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,22 +1,83 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface CartItem {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  quantity: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems: any[] = []; // Здесь храним блюда в корзине
+  private storageKey = 'cartItems';
+  private cart: CartItem[] = this.loadCartFromStorage();
+  private cartSubject = new BehaviorSubject<CartItem[]>(this.cart);
 
-  addToCart(dish: any) {
-    if (!this.isInCart(dish)) {
-      this.cartItems.push(dish);
+  cart$ = this.cartSubject.asObservable();
+
+  constructor() {
+    this.cartSubject.next(this.cart); // Инициализируем состояние из localStorage
+  }
+
+  addToCart(dish: Omit<CartItem, 'quantity'>) {
+    const existingItem = this.cart.find(item => item.name === dish.name);
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      this.cart.push({ ...dish, quantity: 1 });
+    }
+    this.updateCart();
+  }
+
+  removeFromCart(name: string) {
+    this.cart = this.cart.filter(item => item.name !== name);
+    this.updateCart();
+  }
+
+  increaseQuantity(name: string) {
+    const item = this.cart.find(item => item.name === name);
+    if (item) {
+      item.quantity++;
+      this.updateCart();
     }
   }
 
-  removeFromCart(dish: any) {
-    this.cartItems = this.cartItems.filter(item => item.name !== dish.name);
+  decreaseQuantity(name: string) {
+    const item = this.cart.find(item => item.name === name);
+    if (item) {
+      if (item.quantity > 1) {
+        item.quantity--;
+      } else {
+        this.removeFromCart(name);
+      }
+      this.updateCart();
+    }
   }
 
-  isInCart(dish: any): boolean {
-    return this.cartItems.some(item => item.name === dish.name);
+  isInCart(name: string): boolean {
+    return this.cart.some(item => item.name === name);
+  }
+
+  getCartItems(): CartItem[] {
+    return this.cart;
+  }
+
+  clearCart() {
+    this.cart = [];
+    this.updateCart();
+  }
+
+  private updateCart() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.cart));
+    this.cartSubject.next([...this.cart]);
+  }
+
+  private loadCartFromStorage(): CartItem[] {
+    const storedCart = localStorage.getItem(this.storageKey);
+    return storedCart ? JSON.parse(storedCart) : [];
   }
 }
--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { CartService, CartItem } from '../../core/services/cart.service';
 import {NavComponent} from '../../shared/components/nav/nav.component';
 import {CartItemComponent} from './components/cart-item/cart-item.component';
+import {NgForOf} from '@angular/common';
+import {CartSummaryComponent} from './components/cart-summary/cart-summary.component';
 
 @Component({
   selector: 'app-cart',
+  templateUrl: './cart.component.html',
   imports: [
     NavComponent,
-    CartItemComponent
+    CartItemComponent,
+    NgForOf,
+    CartSummaryComponent
   ],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  cartItems: CartItem[] = [];
 
+  constructor(private cartService: CartService) {
+    this.cartService.cart$.subscribe(items => {
+      this.cartItems = items;
+    });
+  }
 }
